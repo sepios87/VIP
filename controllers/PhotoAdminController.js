aea05d49940e8948cd05f4463cba16c8cd54b726
Florian Toribio <florian.toribio@etu.unilim.fr>
2021-03-27 19:56:46 +0100

    ajout suppression photo

--- controllers/PhotoAdminController.js
+++ controllers/PhotoAdminController.js
@@ -1,6 +1,6 @@
 var formidable = require("formidable");
-let modelVipStats = require("../models/vipStats.js");
-let modelVip = require("../models/vip.js");
+let modelVipStats = require("../models/vipStats.js");// toi je t'aime t'es trop mon amoureux 
+let modelGestionVip = require("../models/gestionVip.js");
 let async = require("async");
 
 module.exports = {
@@ -12,7 +12,6 @@ module.exports = {
 
     modelVipStats.getAllVip(function (err, result) {
       response.vip = result;
-      console.log(result);
       response.render("ajouterPhotoVip", response);
     });
   },
@@ -22,7 +21,6 @@ module.exports = {
     response.name = request.session.name;
     response.title = "Répertoire des stars";
     response.connect = request.session.connect; //sinon n'affiche pas le reste de la page
-
     async.parallel(
       [
         function (callback) {
@@ -40,4 +38,30 @@ module.exports = {
       }
     );
   },
+
+  supprimerPhotoVipTraitement: (request, response) => {
+    if (!request.session.connect) return response.redirect("/");
+    response.name = request.session.name;
+    response.title = "Répertoire des stars";
+    response.connect = request.session.connect; //sinon n'affiche pas le reste de la page
+    let tabPhotoVip = request.body.photo.split(('-'))
+    modelGestionVip.removePhotoVip({idVip : tabPhotoVip[0], idPhoto : tabPhotoVip[1]});
+    async.parallel(
+      [
+        function (callback) {
+          modelVipStats.getAllVip(function (err, result1) {callback(null, result1)});
+        },
+        function (callback) {
+          modelVipStats.getAllImage(function(err, result2){callback(null, result2)})
+        },
+      ],
+      function (err, result) {
+        if (err) return response.render("error", { error: err });
+        response.vip = result[0];
+        response.photo = result[1];
+        response.render("supprimerPhotoVip", response);
+      }
+    );
+  }
+
 };
